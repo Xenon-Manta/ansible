4f583882d38671bf53e79a1caca8b5da7a941d5a
Rob Saffell <48562680+Xenon-Manta@users.noreply.github.com>
2020-06-05 10:53:17 -0500

    fixed minor issue

--- ansibleInstall.sh
+++ ansibleInstall.sh
@@ -2,7 +2,7 @@ dnf -y install epel-release
 dnf -y install python3
 dnf -y install python3-pip
 dnf config-manager --set-enabled Power-Tools
-dnf install sshpass
+dnf -y install sshpass
 dnf update
-pip3 install ansible
+pip3 -y install ansible
 
